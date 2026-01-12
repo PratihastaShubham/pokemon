a949cb1f3dd0d64216bc9e7e001a5ed8957a818c
PratihastaShubham <shubhampratihasta@gmail.com>
2026-01-12 13:15:11 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,8 @@
 import { MetadataRoute } from 'next'
 
+// This forces the file to be static for Cloudflare Pages
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
     const baseUrl = 'https://pokemonreminiscencia.site'
 
